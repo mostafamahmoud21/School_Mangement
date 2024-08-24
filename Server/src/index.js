d352697a174077c3d51a111db2594d99a4989074
mostafamahmoud21 <152217975+mostafamahmoud21@users.noreply.github.com>
2024-08-25 00:50:45 +0300

    push

--- Server/src/index.js
+++ Server/src/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const connection = require('./config/db.js');
 const schoolRouter = require('./routes/school.router.js');
 const dotenv = require('dotenv').config();
-
+const cors=require('cors')
+const compression=require('compression')
 const app = express()
+
 app.use(express.json())
+app.use(cors())
+app.use(compression())
 app.use('/api', schoolRouter)
 
 app.listen(process.env.PORT, () => {
